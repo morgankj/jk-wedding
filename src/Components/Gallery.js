2d566cba82de2ba899628b41d8c42349596f697d
Morgan Jones <morgan.kjones95@gmail.com>
2023-04-17 23:51:09 -0500

    Removed broken code for gallery modal. Starting fresh with modal behavior

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -3,41 +3,6 @@ import styled from "styled-components";
 import { adventureArr, engagementArr } from "./ImageArray";
 import GalleryPic from "./GalleryPic";
 
-
-
-
-const modal = document.getElementsByClassName("imageModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-let imgs = document.getElementsByClassName("galleryPic");
-
-Array.from(imgs).forEach(img => {
-  img.onclick = function(){
-    console.log("Clicked picture");
-    // modal.style.display = "block";
-    // modalImg.src = this.src;
-    // captionText.innerHTML = this.alt;
-  }
-});
-
-console.log("Img: ", imgs[0]);
-// const modalPicture = document.getElementByClassName("img01");
-// const captionText = document.getElementByClassName("imageCaption");
-
-
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-
-
-
-
-
-
 export default function Gallery() {
   return (
     <StyledGallery>
@@ -53,11 +18,6 @@ export default function Gallery() {
         {adventureArr.map((obj) => (
           <GalleryPic key={obj.id} picture={obj} />
         ))}
-      </div>
-      <div className="imageModal">
-        <span className="close">&times;</span>
-        <img className="modalPicture" alt="Jon and Morgan"/>
-        <div className="imageCaption"></div>
       </div>
     </StyledGallery>
   );
@@ -78,33 +38,5 @@ const StyledGallery = styled.div`
     display: flex;
     justify-content: space-evenly;
     flex-wrap: wrap;
-  }
-  .imageModal {
-    display: none;
-    position: fixed;
-    z-index: 1;
-    padding-top: 100px; /* Location of the box */
-    left: 0;
-    top: 0;
-    width: 100%; /* Full width */
-    height: 100%; /* Full height */
-    overflow: auto; /* Enable scroll if needed */
-    background-color: rgb(0, 0, 0); /* Fallback color */
-    background-color: rgba(0, 0, 0, 0.9); /* Black w/ opacity */
-  }
-  .close {
-    position: absolute;
-    top: 15px;
-    right: 35px;
-    color: #fefae0;
-    font-size: 40px;
-    font-weight: bold;
-    transition: 0.3s;
-  }
-  .close:hover,
-  .close:focus {
-    color: #dda15e;
-    text-decoration: none;
-    cursor: pointer;
   }
 `;
